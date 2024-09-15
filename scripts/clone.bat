e28951b853d947c01924210516896122e00cf42a
stimjannik <tim.j.schmidt@web.de>
2024-09-15 16:11:12 +0200

    initial setup

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch main %%A
+for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_error_masking %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch main < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_error_masking < scripts/repo.txt
